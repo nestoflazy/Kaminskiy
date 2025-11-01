08052ba8a2a94f26397b2bdc10976a969c7058cf
Elena Zimakova <ahes@mail.ru>
2025-11-01 19:52:44 +0300

    feat: add adaptive

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
 const team_swiper = new Swiper(".team_swiper", {
-  slidesPerView: 3,
-  spaceBetween: 16,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
   loop: false,
+  freeMode: true,
 
   navigation: {
     nextEl: ".custom-next",
     prevEl: ".custom-prev",
   },
+  breakpoints: {
+    1200: {
+      slidesPerView: 'auto',
+      spaceBetween: 0,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    360: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  }
+  
 });
 
 const services_swiper = new Swiper(".services_swiper", {
-  slidesPerView: 3,
-  spaceBetween: 16,
+  slidesPerView: 1,
+  spaceBetween: 10,
   loop: false,
 
   navigation: {
     nextEl: ".custom-next",
     prevEl: ".custom-prev",
   },
+  breakpoints: {
+    965: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  }
 });
 
 const reviews_swiper = new Swiper(".reviews_swiper", {
-  slidesPerView: 3,
-  spaceBetween: 16,
+  slidesPerView: 1,
+  spaceBetween: 10,
   loop: false,
 
   navigation: {
     nextEl: ".custom-next",
     prevEl: ".custom-prev",
   },
+  breakpoints: {
+    965: {
+      slidesPerView: 3,
+      spaceBetween: 16,
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  }
 });
+
 
 function closeAccordion(button) {
   button.addEventListener("click", (e) => {
